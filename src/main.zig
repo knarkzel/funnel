f3fe94208973b4585ff879e3ea281dcae2438a53
Knarkzel <knarkzel@gmail.com>
2023-06-01 02:10:01 +0200

    Allocator works well

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
 const Console = @import("driver/Console.zig");
 const allocator = @import("kernel/Allocator.zig").allocator;
 
 pub fn main() !void {
-    var numbers = .{ 1, 2, 3, 4, 5 };
-    const slice = try std.fmt.allocPrint(allocator, "{any}", .{numbers});
+    var numbers = ArrayList(u32).init(allocator);
+    try numbers.append(1);
+    try numbers.append(2);
+    try numbers.append(3);
+    const slice = try std.fmt.allocPrint(allocator, "{any}", .{numbers.items});
     Console.write(slice);
 }
